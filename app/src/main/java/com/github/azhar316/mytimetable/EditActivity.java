71e8df4d5c3ce9a373971be4b19b9dce544182bb
Syed Mohammed Azharuddin <sayed.azharudin@gmail.com>
2020-06-28 21:59:21 +0530

    feat:Add views to EditActivity

--- app/src/main/java/com/github/azhar316/mytimetable/EditActivity.java
+++ app/src/main/java/com/github/azhar316/mytimetable/EditActivity.java
@@ -3,10 +3,7 @@ package com.github.azhar316.mytimetable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 public class EditActivity extends AppCompatActivity {
 
@@ -15,13 +12,6 @@ public class EditActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
 
-        FloatingActionButton fab = findViewById(R.id.fab_save);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 }
